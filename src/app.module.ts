42e59ba6f94e3f37d6f40dd6b1bf2540f49d8eb9
wyle-timing-xx <wyle-timing@outlook.com>
2025-11-09 17:37:55 +0800

    添加应用主模块 app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TranscriptionModule } from './transcription/transcription.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TranscriptionModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
